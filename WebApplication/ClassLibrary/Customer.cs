35cff7cecc23144a46cc6f4351d856c67387a6d1
Richard Wysocki <richardwysocki@gmail.com>
2017-04-05 17:36:34 -0400

    Checkin install Project- Testing

---
+++ WebApplication/ClassLibrary/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary
+{
+    public class Customer
+    {
+        private readonly string _firstName;
+        private readonly string _lastName;
+
+        public Customer(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string Name()
+        {
+            return _firstName + ", "+ _lastName;
+        }
+
+    }
+}
---
+++ WebApplication/WebApplication.Tests/Business/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+using ClassLibrary;
+using NUnit.Framework;
+
+namespace WebApplication.Tests.Business
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            //Act
+            var customer = new Customer("Richard","Wysocki");
+            //Assert
+            Assert.That(customer.Name()=="Richard, Wysocki", $"This should be Right: {customer.Name()}");
+        }
+    }
+}
